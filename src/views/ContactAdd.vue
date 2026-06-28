da6383a2e7f0278e358bc618edde9d4cea577ceb
vothaihoangkhanh <vothaihoangkhanh234@gmail.com>
2026-06-28 22:06:25 +0700

    them chuc nang them contact

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="contact" class="page">
+    <h4>Thêm liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="createContact" />
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+    };
+  },
+  methods: {
+    async createContact(data) {
+      await ContactService.create(data);
+
+      this.$router.push({
+        name: "contactbook",
+      });
+    },
+  },
+};
+</script>
